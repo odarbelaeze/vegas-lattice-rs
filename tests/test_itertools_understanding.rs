1297ad6b6a4f9ff37796bb90ab48aaf6153f662d
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-05-20 19:23:39 -0500

    Test itertools

---
+++ tests/test_itertools_understanding.rs
@@ -0,0 +1,34 @@
+#[macro_use] extern crate itertools;
+
+use itertools::{Itertools, cons_tuples};
+
+
+#[test]
+fn itertools_over_a_two_by_two_lattice() {
+    assert_eq!(
+        vec![(0, 0), (0, 1), (1, 0), (1, 1)],
+        iproduct!(0..2, 0..2).collect::<Vec<_>>()
+    );
+}
+
+
+#[test]
+fn cartesian_products_by_hand() {
+    assert_eq!(
+        vec![(0, 0), (0, 1), (1, 0), (1, 1)],
+        (0..2).cartesian_product(0..2).collect::<Vec<_>>()
+    );
+}
+
+
+#[test]
+fn triple_cartesian_products_by_hand() {
+    assert_eq!(
+        iproduct!(0..2, 0..2, 0..2).collect::<Vec<_>>(),
+        cons_tuples(
+            (0..2)
+                .cartesian_product(0..2)
+                .cartesian_product(0..2)
+        ).collect::<Vec<_>>()
+    );
+}
