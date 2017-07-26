31e6569d32b5ebe34b7dc0b432ca441ac1dab924
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-07-26 22:48:36 +0200

    Implement drop command

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::io::{self, Read};
 
 use docopt::{ArgvMap, Docopt};
-use vegas_lattice::Lattice;
+use vegas_lattice::{Axis, Lattice};
 
 
 const USAGE: &'static str = "
@@ -14,7 +14,7 @@ Vegas lattice.
 
 Usage:
     vegas-lattice check [--compressed] [<input>]
-    vegas-lattice drop [-x -y -z] [<input>]
+    vegas-lattice drop [-x -y -z] [--compressed] [<input>]
     vegas-lattice (-h | --help)
     vegas-lattice --version
 
@@ -24,22 +24,59 @@ Options:
 ";
 
 
-fn check(args: ArgvMap) -> Result<(), Box<std::error::Error>> {
+fn read(input: &str) -> Result<Lattice, Box<std::error::Error>> {
     let mut data = String::new();
-    if !args.get_str("<input>").is_empty() {
-        let mut file =
-            File::open(args.get_str("<input>"))?;
+    if !input.is_empty() {
+        let mut file = File::open(input)?;
         file.read_to_string(&mut data)?;
     } else {
         io::stdin().read_to_string(&mut data)?;
     };
     let lattice: Lattice = data.parse()?;
-    if args.get_bool("--compressed") {
+    Ok(lattice)
+}
+
+
+fn write(lattice: Lattice, compressed: bool) {
+    if compressed {
         println!("{}", serde_json::to_string(&lattice).unwrap());
     } else {
         println!("{}", serde_json::to_string_pretty(&lattice).unwrap());
     }
+}
+
+
+fn check(args: ArgvMap) -> Result<(), Box<std::error::Error>> {
+    let lattice = read(args.get_str("<input>"))?;
+    write(lattice, args.get_bool("--compressed"));
     Ok(())
+}
+
+
+fn drop(args: ArgvMap) -> Result<(), Box<std::error::Error>> {
+    let mut lattice = read(args.get_str("<input>"))?;
+    if args.get_bool("-x") {
+        lattice = lattice.drop(Axis::X);
+    }
+    if args.get_bool("-y") {
+        lattice = lattice.drop(Axis::Y);
+    }
+    if args.get_bool("-z") {
+        lattice = lattice.drop(Axis::Z);
+    }
+    write(lattice, args.get_bool("--compressed"));
+    Ok(())
+}
+
+
+fn check_error(res: Result<(), Box<std::error::Error>>) {
+    match res {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        },
+        _ => {},
+    }
 }
 
 
@@ -49,12 +86,10 @@ fn main() {
         .unwrap_or_else(|e| e.exit());
 
     if args.get_bool("check") {
-        check(args).unwrap_or_else(|e| {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        });
+        check_error(check(args));
+    } else if args.get_bool("drop") {
+        check_error(drop(args));
     } else {
         println!("{:?}", args);
     }
-
 }
